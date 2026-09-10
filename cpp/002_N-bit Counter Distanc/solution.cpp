98aa12cfebabaf4ca46d96d6303c188fdcf0de89
Yiningwang0421 <156242937+Yiningwang0421@users.noreply.github.com>
2026-09-10 16:40:40 -0400

    Correct forwardDistance function implementation
    Fix function definition for forwardDistance and handle end < start case.

--- cpp/002_N-bit Counter Distanc/solution.cpp
+++ cpp/002_N-bit Counter Distanc/solution.cpp
@@ -1,7 +1,17 @@
 #include <cstdint>
+#include <limits>
 
 uint64_t forwardDistance(
     uint64_t start,
     uint64_t end,
     unsigned width
-);
+){
+    if(end < start){
+        if(width == 64 ){            
+            return end + (UINT64_MAX - start) + 1;
+        }else{
+            end = end + (1ULL << width);
+        }
+    }
+    return end -start;
+}
